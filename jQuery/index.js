470b50be09591dc30e45ce07595cea03d98eff92
LucasYuNju <LucasYuNju@gmail.com>
2016-08-17 22:52:01 +0800

    add next & prev

--- jQuery/index.js
+++ jQuery/index.js
@@ -2,7 +2,8 @@
     const w = window, d = document;
     const sakuraPrototype = {
         forEach: [].forEach,
-        length: [].length,
+        length: 0,
+        selector: '',
         splice: [].splice,
         addClass: function(value) {
             var reg = new RegExp('(\\s|^)' + value + '(\\s|$)');
@@ -20,7 +21,18 @@
                 }
                 dom.style[attribute] = value;
             });
-        }
+            return this;
+        },
+        next: function() {
+            let current = this;
+            while ((current = current.nextSibling) && current.nodeType !== 1) {}
+            return current;
+        },
+        prev: function() {
+            let current = this;
+            while ((current = current.previousSibling) && current.nodeType !== 1) {}
+            return current;
+        },
         text: function(value) {
             this.forEach(dom => {
                 if (arguments.length === 0)
@@ -29,8 +41,7 @@
                 }
                 dom.innerText = value;
             });
-        }
-
+        },
     };
     const sakura = function(selector) {
         const doms = d.querySelectorAll(selector);
