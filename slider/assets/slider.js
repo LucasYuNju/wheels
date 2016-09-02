6f2af6e6341dd5efd6c0a1d7d93fa6e8291e169d
LucasYuNju <LucasYuNju@gmail.com>
2016-09-02 10:45:06 +0800

    runnable version of slider.

--- slider/assets/slider.js
+++ slider/assets/slider.js
@@ -5,28 +5,29 @@ class Slider {
         this.numSlides = 3;
         this.currentSlide = 0;
         this.frameTime = 20;
-        this.pauseTime = 3000;
+        this.pauseTime = 2000;
+        this.duration = 200;
     }
     
     switchTo(target) {
-        this.currentSlide = target;
-        this.begin = styleOf(this.element).left;
-        this.translation = -slideWidth * (target - this.currentSlide);
-        this.duration = 200;
+        console.log("switch to " + target);
+        this.begin = parseInt(styleOf(this.element).left);
+        this.translation = -this.slideWidth * (target - this.currentSlide);
         this.time = 0;
+        this.currentSlide = target;
         this.tick();
     }
     
     _switchToNext() {
-        this.switchTo(this.currentSlide + 1);
+        this.switchTo((this.currentSlide + 1) % this.numSlides);
     }
     
     tick() {
         clearTimeout(this.timer);
-        if (this.translation && this.time < self.duration) {
+        if (this.translation && this.time < this.duration) {
             const dest = Tween.Quad.easeOut(this.time, this.begin, this.translation, this.duration);
+            this._moveTo(Math.round(dest));
             this.time += this.frameTime;
-            this._moveTo(dest);
             this.timer = setTimeout(this.tick.bind(this), this.frameTime);            
         }
         else {
