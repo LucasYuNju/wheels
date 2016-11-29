544bc8f280b72ae5a57b877f1bd462efe772e1f7
LucasYuNju <LucasYuNju@gmail.com>
2016-11-29 14:36:08 +0800

    Redux-slim

---
+++ redux-slim/README.md
@@ -0,0 +1,7 @@
+# Motivation
+
+受[gaearon/slim-redux.js](https://gist.github.com/gaearon/ffd88b0e4f00b22c3159)的启发，从零实现redux的基本功能。
+
+redux的代码大量使用了函数式编程，像middleware，createStoreEnhancer等概念，直接看文档难以理解。
+
+代码完成之后会写一个注释版本。
---
+++ redux-slim/index.js
@@ -0,0 +1,88 @@
+function mapValues(initial, fn) {
+    return Object.keys(initail).reduce((result, key) => {
+        result[key] = fn(initial[key], key);
+        return result;
+    }, {});
+}
+
+// Simplify actionCreator invocation
+export function bindActionCreators(actionCreators, dispatch) {
+    return mapValues(actionCreators, (actionCreator) => {
+        return (...args) => dispatch(actionCreator(...args));
+    });
+}
+
+// Assume that function arguments are x, y, z, returned function is (...args) => x(y(z(...args))).
+export function comopse(...methods) {
+    return (args) => methods.reverse().reduce((result, method) => method(...result), args);
+}
+
+export function combineReducers(reducers) {
+    return (state = {}, action) => mapValues(reducers, (reducer, key) => {
+        reducer(state[key], action);
+    })
+}
+
+// returns a store(createStore) enhancer
+// Quoted from redux doc:
+// To ensure that you may only apply middleware once, applyMiddleware operates on createStore() rather than on store itself. 
+// Instead of (store, middlewares) => store, its signature is (...middlewares) => (createStore) => createStore.
+export function applyMiddleware(...middlewares) {
+    return (createStore) => (preloadedState, reducer, enhancer) => {
+        const store = createStore(preloadedState, reducer, enhancer);
+        middlewareAPI = {
+            dispatch: store.dispatch,
+            getState: store.getState,
+        };
+        // signature of middleware: store => next => action => {}
+        chain = middlewares.map(middleware => {
+            return middleware(middlewareAPI);
+        });
+        const dispatch = compose(...chain)(dispatch);
+        return { ...store, dispatch };
+    };
+}
+
+export function createStore(initialState, reducer) {
+    const listeners = [];
+    let isDispatching = false;
+    let state = initialState;
+    
+    function dispatch(action) {
+        if (isDispatching) {
+            throw new Error('Reducer may not dispatch action');
+        }
+        try {
+            isDispatching = true;
+            const state = reducer(state, action);
+        }
+        finally {
+            isDispatching = false;
+        }
+        listeners.forEach(listener);
+        return action;
+    }
+    
+    function getState() {
+        return state;
+    }
+    
+    function replaceReducer(newReducer) {
+        reducer = newReducer;
+    }
+    
+    function subscribe(callback) {
+        listeners.push(callback);
+        
+        return function unsubscribe() {
+            listeners.splice(listeners.indexOf(callback), 1);
+        }
+    }
+    
+    return {
+        dispatch,
+        getState,
+        replaceReducer,
+        subscribe,
+    };
+}
