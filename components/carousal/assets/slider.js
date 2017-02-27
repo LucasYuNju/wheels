254a685b76910b79a34f8736fe4be20877d589c8
LucasYuNju <LucasYuNju@gmail.com>
2017-02-27 10:39:26 +0800

    Update carousal

--- components/carousal/assets/slider.js
+++ components/carousal/assets/slider.js
@@ -1,42 +1,63 @@
 class Slider {
-    constructor(selector) {
-        this.element = document.querySelector(selector);
-        this.slideWidth = 440;
-        this.numSlides = 3;
-        this.currentSlide = 0;
-        this.frameTime = 20;
-        this.pauseTime = 2000;
-        this.duration = 200;
+  constructor(selector) {
+    this.element = document.querySelector(selector);
+    this.slideWidth = 440;
+    this.length = 5;
+    this.step = 1;
+
+    this.frameTime = 20;
+    this.displayTime = 2000;
+    this.animDuration = 200;
+
+    setInterval(this.next.bind(this), this.displayTime);
+  }
+
+  next() {
+    this.switchTo((this.step + 1) % this.length);
+  }
+
+  switchTo(target) {
+    console.log("switch to " + target);
+    this.begin = parseInt(window.getComputedStyle(this.element).left);
+    this.transition = -this.slideWidth * (target - this.step);
+    this.animTime = 0;
+    this.step = target;
+    this.tick();
+  }
+
+  tick() {
+    if (this.transition && this.animTime < this.animDuration) {
+      const dest = Tween.Quad.easeOut(this.animTime, this.begin, this.transition, this.animDuration);
+      this._moveTo(Math.round(dest));
+      this.animTime += this.frameTime;
+      this.timer = setTimeout(this.tick.bind(this), this.frameTime);
     }
-    
-    switchTo(target) {
-        console.log("switch to " + target);
-        this.begin = parseInt(styleOf(this.element).left);
-        this.translation = -this.slideWidth * (target - this.currentSlide);
-        this.time = 0;
-        this.currentSlide = target;
-        this.tick();
+    else {
+      this._moveTo(this.begin + this.transition);
+      if (this.step === this.length - 1) {
+        this.step = 1;
+        this._moveTo(1);
+      }
+      clearTimeout(this.timer);
+      // TODO，这个完全可以用interval
     }
-    
-    _switchToNext() {
-        this.switchTo((this.currentSlide + 1) % this.numSlides);
+  }
+
+  _moveTo(x) {
+    this.element.style.left = x + "px";
+  }
+}
+
+const Tween = {
+  Quad: {
+    /*
+    t: current time（当前时间）；
+    b: beginning value（初始值）；
+    c: change in value（变化量）；
+    d: duration（持续时间）。
+    */
+    easeOut: function(t,b,c,d) {
+      return -c * ((t=t/d-1)*t*t*t - 1) + b;
     }
-    
-    tick() {
-        clearTimeout(this.timer);
-        if (this.translation && this.time < this.duration) {
-            const dest = Tween.Quad.easeOut(this.time, this.begin, this.translation, this.duration);
-            this._moveTo(Math.round(dest));
-            this.time += this.frameTime;
-            this.timer = setTimeout(this.tick.bind(this), this.frameTime);            
-        }
-        else {
-            this._moveTo(this.begin + this.translation);
-            this.timer = setTimeout(this._switchToNext.bind(this), this.pauseTime);
-        }
-    }
-    
-    _moveTo(x) {
-        this.element.style.left = x + "px";
-    }
+  }
 }
