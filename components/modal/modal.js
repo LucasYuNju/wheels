980ec47898c08272e63463ef5e74fa3cb013b39a
LucasYuNju <LucasYuNju@gmail.com>
2017-03-09 15:16:47 +0800

    Update modal

--- components/modal/modal.js
+++ components/modal/modal.js
@@ -1,5 +1,6 @@
 // 这里只是最简单的实现，完整的接口，应该包含显示时间，用于关闭的函数，以及被关闭时的回调函数，以及窗口宽高等
 window.createModel = function({
+  title,
   message,
   parentSelector = 'body',
 }) {
@@ -8,16 +9,17 @@ window.createModel = function({
     return;
   const template = `
     <div class="modal-content">
+      <div class="header">${title}<span class="close-button">+</div>
       <div class="message">${message}</div>
       <div class="buttons">
-        <button class="close-button">关闭</button>
+        <button class="confirm-button">确认</button>
+        <button class="cancel-button">关闭</button>
       </div>
     </div>
   `;
   // 用jQuery会更简单一些
   const modal = document.createElement('div');
   modal.classList.add('modal')
-  // modal.innerHTML = template.replace('{message}', message);
   modal.innerHTML = template;
   parent.appendChild(modal);
 
@@ -25,6 +27,12 @@ window.createModel = function({
     if (e.target === modal) {
       parent.removeChild(modal);
     }
+  });
+  document.querySelector('.modal .cancel-button').addEventListener('click', (e) => {
+    parent.removeChild(modal);
+  });
+  document.querySelector('.modal .confirm-button').addEventListener('click', (e) => {
+    parent.removeChild(modal);
   });
   document.querySelector('.modal .close-button').addEventListener('click', (e) => {
     parent.removeChild(modal);
